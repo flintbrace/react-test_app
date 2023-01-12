a2d3d8a122b64270d9b803d114da845762f392cf
flintbrace <bracejeff150@gmail.com>
2023-01-12 11:57:16 +0000

    eeed

--- src/Header.js
+++ src/Header.js
@@ -2,8 +2,6 @@ const Header = ({ title }) => {
   return (
     <header>
       <h1>{title}</h1>
-      <p>Here's what We'll be Modifying</p>
-      <p>Here's our First Modification</p>
     </header>
   );
 };
